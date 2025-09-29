ef62c758a850d4c498779f7b33ebb00deabed1b1
Edu <kaedu18@gmail.com>
2025-09-28 19:39:13 -0500

    Implement new features and update endpoint

--- src/utils/fetchRadios.ts
+++ src/utils/fetchRadios.ts
@@ -2,7 +2,9 @@ import axios from "axios"
 import { RadiosSchema } from "../schema/RadioSchema"
 
 export const FetchApiRadios = async () => {
-    const url = 'https://de1.api.radio-browser.info/json/stations/bycountry/Peru'
+    // const url = 'https://de1.api.radio-browser.info/json/stations/bycountry/Peru'
+    const url = 'http://37.27.202.89/json/stations/bycountrycodeexact/PE'
+    // const url = process.env.VITE_API_KEY
     const { data } = await axios(url)
     const result = RadiosSchema.safeParse(data)
     if (result.success) return result.data
